9802dad240c30fed1e50c2a7f3a388d3b51b2c19
seconduser <user2@gmail.com>
2018-07-17 13:49:32 +0530

    Changes from second

--- GitTestApp/src/main/java/com/servlets/TestServlets.java
+++ GitTestApp/src/main/java/com/servlets/TestServlets.java
@@ -18,6 +18,7 @@ public class TestServlets extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Changes From First User..!");
+		System.out.println("Changes from second user..!");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
