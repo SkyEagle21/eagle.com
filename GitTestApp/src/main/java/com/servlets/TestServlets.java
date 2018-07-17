aa22902341a989c6cc0ff3103a2b57c3e2a20553
seconduser <user2@gmail.com>
2018-07-17 13:46:23 +0530

    Final commit from user1

--- GitTestApp/src/main/java/com/servlets/TestServlets.java
+++ GitTestApp/src/main/java/com/servlets/TestServlets.java
@@ -18,7 +18,6 @@ public class TestServlets extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Changes From First User..!");
-		System.out.println("Changes From Second User");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
